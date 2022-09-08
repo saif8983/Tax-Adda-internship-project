1fdfc7d2f0751b528e667d1bc8edc23e9f4c26f2
saif8983 <asaif8367@gmail.com>
2022-09-08 13:45:43 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Homecomponent from './Components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class="container">
+  <div class="side-black-container">
+    <div class="nav-black-container">hi</div>
+  </div>
+  hi
+  <div class="nav-black-containre">saif</div>
+  <div class="small-navbar">small</div>
+  <div class="main-container">main</div>
+</div>
+    </>
+  );
+}
+
+export default App;
